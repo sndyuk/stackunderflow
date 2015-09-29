9fe499f23159634a7980d60416c9022af0d92ec8
Yuki Sanada <sanada@sndyuk.com>
2015-09-29 15:49:16 -0400

    Add soft delete behavior to User by Paranoia

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
+  acts_as_paranoid
+
   has_many :own_questions, foreign_key: :owner_id, class_name: Question
+  has_many :own_comments, foreign_key: :owner_id, class_name: Comment
 end
