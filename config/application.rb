edc7eb90b1c04f941e161a5a182dd2870bc3db75
Yuki Sanada <sanada@sndyuk.com>
2015-10-01 12:37:43 -0400

    Enable tail call optimization

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,22 @@ require 'rails/all'
 
 Bundler.require(*Rails.groups)
 
+RubyVM::InstructionSequence.compile_option = {
+  inline_const_cache: true,
+  instructions_unification: true,
+  operands_unification: true,
+  peephole_optimization: true,
+  specialized_instruction: true,
+  stack_caching: false,
+  tailcall_optimization: true,
+  trace_instruction: false
+}
+puts '------------------------------------------------'
+puts '---------- RubyVM Compile Options --------------'
+puts '------------------------------------------------'
+RubyVM::InstructionSequence.compile_option.each { |k, v| puts("#{k}##{v}") }
+puts '------------------------------------------------'
+
 module Stackunderflow
   class Application < Rails::Application
     # 環境ごとに設定を上書きするためのファイルはこちらへ: config/environments/*.rb
